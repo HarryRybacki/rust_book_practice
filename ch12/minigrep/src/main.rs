be7a1b47b87b8d0ec7f8a6b0988c0c548b471495
Harry Rybacki <hrybacki@gmail.com>
2024-05-04 20:01:05 +0200

    Add builder for Config and improve error handling.

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,8 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for '{}'", config.query);
     println!("In file: {}", config.file_path);
@@ -17,9 +22,15 @@ struct Config {
     query: String,
     file_path: String,
 }
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
 
-    Config { query, file_path }
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments;"); // how does 'return' function and why is this a statement ';'?
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
