3b05d1c06bd510bd9c4d395481fb9a5f8269c42a
Harry Rybacki <hrybacki@gmail.com>
2024-05-04 20:13:20 +0200

    Finished extracting application logic from main() and improved error handling.

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    // report error call process exit if build() fails and unwrap explodes unexpectedly
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
@@ -12,10 +14,19 @@ fn main() {
     println!("Searching for '{}'", config.query);
     println!("In file: {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file.");
+    // report error and call process exit IF Err type is returned unexpectedly
+    if let Err(e) = run(config) {
+        println!("Problem running application: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With contents:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
