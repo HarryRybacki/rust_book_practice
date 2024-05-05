5a4d6afa2f6ad6ac4767162d2b787922772289e5
Harry Rybacki <hrybacki@gmail.com>
2024-05-05 20:11:24 +0200

    Ensure error messages go to stderr not stdout.

--- ch12/minigrep/src/lib.rs
+++ ch12/minigrep/src/lib.rs
@@ -11,7 +11,7 @@ pub struct Config {
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("not enough arguments;"); // how does 'return' function and why is this a statement ';'?
+            return Err("not enough arguments"); // how does 'return' function and why is this a statement ';'?
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -7,13 +7,13 @@ fn main() {
 
     // report error call process exit if build() fails and unwrap explodes unexpectedly
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     // report error and call process exit IF Err type is returned unexpectedly
     if let Err(e) = minigrep::run(config) {
-        println!("Problem running application: {e}");
+        eprintln!("Problem running application: {e}");
         process::exit(1);
     }
 }
