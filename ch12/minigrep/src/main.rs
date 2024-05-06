0ca79d70bffc71a0abfcc8f45f7760b64a994c82
Harry Rybacki <hrybacki@gmail.com>
2024-05-06 11:26:39 +0200

    Move Config logic to use iterators for cleaner flow

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -3,10 +3,8 @@ use std::process;
 
 use minigrep::Config;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
     // report error call process exit if build() fails and unwrap explodes unexpectedly
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
